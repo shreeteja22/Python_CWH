badee01d52395130da7fbf98c7561e26bab1cd14
shreeteja22 <smutukundu2206@gmail.com>
2024-08-02 18:06:10 +0530

    commit

--- pythonusingw3schools.py
+++ pythonusingw3schools.py
@@ -18,11 +18,11 @@
 # a = int(input("Enter your Age : "))
 # print("Your Age is", a)
 
-# for (18<a):
-#     print("You can drive")
-# elif (60>a):
-#     print("You can drive")
-# else():
-# print("You cannot drive")
+for (18<a):
+    print("You can drive")
+elif (60>a):
+    print("You can drive")
+else():
+print("You cannot drive")
 
  
