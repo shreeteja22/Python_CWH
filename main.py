efd71f3fab78596badd6459ee3d09ce66d0e747f
shreeteja22 <smutukundu2206@gmail.com>
2024-08-02 18:02:00 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# name = str(input("ENTER YOUR NAME : "))
+# print("Hello," + name)      
+
+# letter = "Walter white"
+# letter1 = "Mutukundu"
+# letter2 = "Srinivasulu"
+# len1 = len(letter)       #checking length
+# len2 = len(letter1)
+# len3 = len(letter2)
+# print(len1 , len2 , len3)
+
+# letter = "ShreetejaSrinivasuluMutukundu"
+# len1 = len(letter)
+# print(letter[0:20])      #slicing 
+
+# letter = "ShreetejaSrinivasuluMutukundu"
+# len1 = len(letter)            #middle letter slicing
+# print(letter[9:20])   
+
+# # upper case ka method
+# a = "Harry!"
+# print(len(a))       #(letter keywords)
+# print(a.upper())
+# print(a.replace("Harry", "Teja"))   #letter keywords
+# print(a.rstrip("!"))
+
+# a = "harry ! Teja Teja Teja Teja Teja !"
+# # print(a.split(" "))
+# # print(a.capitalize())   #capitalise starting letter ko capital karke baki sb small kardeta
+# # print(a.count("Teja"))
+# print(a.endswith("Reddy"))   #ye value Boolean(true aur false) mai hi deta ...agar endswith(teja) hai toh true ...if not then false
---
+++ pythonusingw3schools.py
@@ -0,0 +1,28 @@
+# x = "Awesome"
+
+# def funct():
+#     x = "Easy"
+#     print("Python is "  + x)
+
+# funct()
+
+# print("Java too is " + x)
+# a = "lorem30"
+# print(a)
+# python = print(a)
+# print(python)
+# for x in "shreeteja":
+#   print(x) 
+
+
+# a = int(input("Enter your Age : "))
+# print("Your Age is", a)
+
+# for (18<a):
+#     print("You can drive")
+# elif (60>a):
+#     print("You can drive")
+# else():
+# print("You cannot drive")
+
+ 
