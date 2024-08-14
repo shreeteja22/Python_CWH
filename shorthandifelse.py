f055bd492cffe9fae6f253fe8d4afa5d985482b4
shreeteja22 <smutukundu2206@gmail.com>
2024-08-14 23:11:08 +0530

    shorthandifelse

---
+++ shorthandifelse.py
@@ -0,0 +1,5 @@
+a = 330
+b = 345
+print("A>B") if a>b else print("A>B") if b<a else print("B>A")
+
+#agar chota lines of code mai likhna hai aur space nahi hai phir short hand if else statements use kar sakte ...if statement is read backwards
