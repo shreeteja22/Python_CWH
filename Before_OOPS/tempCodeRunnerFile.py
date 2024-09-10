2957e1a85734bdbfd9dd55a1ef541332034dd2f4
shreeteja22 <smutukundu2206@gmail.com>
2024-09-10 09:38:14 +0530

    python

---
+++ Before_OOPS/tempCodeRunnerFile.py
@@ -0,0 +1,17 @@
+def fac(n):
+#     if (n==0 or n==1):
+#         return(1)
+#     else:
+#         return n * fac(n-1)
+    
+# print(fac(1))
+# print(fac(2))
+# print(fac(3))
+# print(fac(4))
+# print(fac(5))
+# print(fac(6))
+# print(fac(7))
+# print(fac(8))
+# print(fac(9))
+# print(fac(10))
+
